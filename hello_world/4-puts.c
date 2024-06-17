8f8ab12023ec3b9b4558f0b8b16229f4ee91c5a7
FullAuto33 <djelloulrouba@yahoo.com>
2024-06-17 09:21:16 +0000

    Fix problem to commentaire main T4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,17 @@
-#include <stdio.h>
 /*
-*C program that prints exactly with puts
+ * Create by djelloul in holberton
 */
+
+#include <stdio.h>
+
+/**
+ * main - Hello,puts
+ *
+ *Description : Print exactly "Programming is like building a
+ *                             multilingual puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
