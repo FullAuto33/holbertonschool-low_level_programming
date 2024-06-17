3600be4e1b59f28470270bfa88765528e75d5c6f
FullAuto33 <djelloulrouba@yahoo.com>
2024-06-17 08:51:11 +0000

    Fix porblem to betty with space T4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-/*
-  Program print a line 
-
- */
-
-
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
