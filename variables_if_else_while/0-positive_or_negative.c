4ecbbee2c2ec1488bfa3cc429c3d15391eebdb71
FullAuto33 <djelloulrouba@yahoo.com>
2024-06-18 08:00:15 +0000

    Fix problem to if loops

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -30,10 +30,11 @@ int main(void)
 
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
 	else if (n < 0)
-		printf("%d is negative \n", n);
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
 	/* your code goes there */
 	return (0);
 }
