47dcca81043c475e601dec76b244c681de72757f
FullAuto33 <djelloulrouba@yahoo.com>
2024-06-19 09:34:02 +0000

    prints all possible combinations of single-digit numbers. T9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+/*
+ * Create by Djelloul in Holberton
+*/
+
+#include <stdio.h>
+
+/**
+ * main - Numbez
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ *
+ * Return: Always 0
+ *
+*/
+
+int main(void)
+{
+int n;/*n est un nombre entier integer*/
+char virgue = ',';
+char space = ' ';
+n = 0;/* n egale a 0*/
+while (n <= 9)/*Tant que n est plus petit a 9*/
+{
+putchar('0' + n);/*affiche n*/
+putchar(virgue);
+putchar(space);
+n = n + 1;/*ajoute +1 a n*/
+}
+putchar('\n');/*Renvoie a la ligne*/
+return (n);/* retourne 0*/
+}
