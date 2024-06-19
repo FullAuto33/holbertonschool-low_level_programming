6ee17267fc13b821e8cd3ddd9576f84c61ef799b
FullAuto33 <djelloulrouba@yahoo.com>
2024-06-19 12:26:07 +0000

    script print command _putchar T0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/*
+ * Create by Djelloul in Holberton
+*/
+
+#include <stdio.h>
+
+/**
+ * main - _putchar
+ *
+ * Description: prints _putchar, followed by a new line.
+ *
+ * Return: Always 0
+ *
+*/
+
+int main(void)
+{
+printf("_putchar");
+putchar('\n');
+return (0);
+}
