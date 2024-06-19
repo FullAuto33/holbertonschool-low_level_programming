d3c3b8a448aa933cad8841ba52aa5b1b1201587d
FullAuto33 <djelloulrouba@yahoo.com>
2024-06-19 07:37:33 +0000

    Create program in c asign n and print last digit T1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -24,22 +24,26 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
+if ((n % 10) != 0 && (n % 10) < 6) /* Si le reste de diviser
+				    * par 10 est different de 0 et inferieur a 6
+				    */
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n"
+/* Affiche last ... n of n reste ...*/
+, n, n % 10); /* remplace %d par n et modulo n*/
+}
+else if ((n % 10) > 5) /* Si le rest de n est plus grand que 5*/
+{
+printf("Last digit of %d is %d and is greater than 5\n"
+/* Affiche last ... n of n rest plus grand que 5*/
+, n, n % 10); /* remplace %d par n et modulo n*/
+}
+else /*Sinon*/
+{
+printf("Last digit of %d is %d and is 0\n"
+/* Affiche last ... n is n reste et 0*/
+, n, n % 10);
+}
 
-	if ((n % 10) > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n",
-			n, n % 10);
-	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-			n, n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n",
-			n, n % 10);
-	}
-
-	return (0);
+return (0);
 }
