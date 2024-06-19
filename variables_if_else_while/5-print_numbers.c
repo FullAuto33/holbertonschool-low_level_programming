f7e50dcb9d907f23f55e289b4b54a54b63a74e89
FullAuto33 <djelloulrouba@yahoo.com>
2024-06-19 08:12:37 +0000

    Print all number T5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -20,8 +20,9 @@ int n;/*n est un nombre entier integer*/
 n = 0;/* n egale a 0*/
 while (n <= 9)/*Tant que n est plus petit a 9*/
 {
-printf(" %d", n);/*affiche n*/
+printf("%d", n);/*affiche n*/
 n = n + 1;/*ajoute +1 a n*/
 }
+putchar('\n');/*Renvoie a la ligne*/
 return (0);/* retourne 0*/
 }
