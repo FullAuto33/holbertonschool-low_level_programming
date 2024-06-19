cd9a3f20d1213a7e514735e1dbd9efb74aa60fd5
FullAuto33 <djelloulrouba@yahoo.com>
2024-06-19 08:55:42 +0000

    Print alphabet a reverse T7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+/*
+ *Create by Djelloul in Holberton
+ */
+#include <stdio.h>
+
+/**
+ * main - I sometimes suffer from insomnia.
+ *
+ * Description: prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+char lettre = 'z'; /* affecte le charactere 'a' a la variable lettre*/
+while (lettre <= 'a') /*Tant que lettre inferieur ou egal a z*/
+{
+putchar(lettre);/*Renvoie le charactere de lettre*/
+lettre--;/*on change de lettre on ajoute 1 a la variable lettre*/
+}
+putchar('\n');/*Renvoie un saut a la ligne*/
+return (0);/*retourne 0*/
+}
