9ed88619218de0f0926aaf92dddfe27afba7504c
FullAuto33 <djelloulrouba@yahoo.com>
2024-06-21 07:34:24 +0000

    function that computes the absolute value of an integer.T6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints the alphabet, in lowercase, followed by a new line.
+ *@r: number to calculate to absolute values
+ * Return: Always 0.
+*/
+
+int _abs(int r)
+{
+if (r >= 0)
+return (r);
+else
+return (-r);
+}
