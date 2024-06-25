b789e9aa5d8ff3280660ca23c4a48f5764c7e868
FullAuto33 <djelloulrouba@yahoo.com>
2024-06-25 09:55:47 +0000

    function that prints a triangle, followed by a new line.T10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -18,14 +18,13 @@ _putchar('\n');
 else
 for (i = 0; i < size; i++)
   {    
-    for (j = 0; j < 2 * (size - i) - 1; j++)
+    for (j = 0; j <= (size - i); j++)
       {
 	_putchar(' ');
       }
     for (j = 0; j <= i; j++)
       {
 	_putchar('#');
-	_putchar(' ');
       }
 _putchar('\n');
 }
