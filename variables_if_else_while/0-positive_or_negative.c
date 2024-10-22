f2850a894aedf1ee02e880d22fb4aae10d754c95
root <djelloulrouba@yahoo.com>
2024-10-22 07:35:44 +0000

    Script print number and positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,23 +2,34 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
 /* betty style doc for function main goes there */
+
+/**
+* main - Print number and positive or negative
+*
+* Return: Always 0 (Success)
+*/
+
+
+
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if ( n > 0 ){
-	  printf("%d is positive\n", n);
-	  }
-	if ( n == 0 ){
-	  printf("%d is zero\n", n);
-	  }
-	if ( n < 0 ){
-	  printf("%d is negative\n", n);
-	  }
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
