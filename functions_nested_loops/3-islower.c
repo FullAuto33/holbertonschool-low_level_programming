6da6bf65bdc53bfb40db046aea9c8a0a507ae09d
root <djelloulrouba@yahoo.com>
2024-10-24 13:15:18 +0000

    Script check if charactere is lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * _islower - check if is lower or no.
+ *
+ * @c : charactere
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+return (0);
+}
