252216f704b5ec91320b11f283b04b2eef243a8b
root <djelloulrouba@yahoo.com>
2024-10-27 17:32:24 +0000

    Script add two integer

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two integer
+ *
+ * @num1 : Number one
+ *
+ * @num2 : Number two
+ *
+ * Return: Always 0.
+ */
+
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
