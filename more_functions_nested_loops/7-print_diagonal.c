b17fe3894c8f8c73c7e97eb2270d7996da8ff954
root <djelloulrouba@yahoo.com>
2024-10-28 14:00:12 +0000

    Script print line

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,6 +11,7 @@
 void print_diagonal(int n)
 {
 int compteur;
+int space;
 if (n <= 0)
 {
 _putchar('\n');
@@ -19,6 +20,10 @@ else
 {
 for (compteur = 0; compteur < n; compteur++)
 {
+for (space = 0; space < n; space++)
+{
+_putchar(' ');
+}
 _putchar('\\');
 }
 _putchar('\n');
