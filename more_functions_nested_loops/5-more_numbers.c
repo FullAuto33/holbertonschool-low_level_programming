e32b4ea6f023ed762af24c5d40f439e2a8d96fd8
root <djelloulrouba@yahoo.com>
2024-10-28 10:44:59 +0000

    Script print more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,7 @@ void more_numbers(void)
 {
 int number;
 int compteur;
-for (compteur = 0; compteur <= 8; compteur++)
+for (compteur = 0; compteur <= 9; compteur++)
 {
 for (number = 0; number <= 14; number++)
 {
