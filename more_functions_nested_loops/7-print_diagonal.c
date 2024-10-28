61fa86558b598939026be8e40299fd9aa08fa3c5
root <djelloulrouba@yahoo.com>
2024-10-28 13:46:01 +0000

    Script print line

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - print linebas
+ *
+ * @n : number of line
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+int compteur;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (compteur = 0; compteur < n; compteur++)
+{
+_putchar('\\');
+}
+_putchar('\n');
+}
+}
