ed0e2c71224e98c54556faecd3384c9ac884c1e7
root <djelloulrouba@yahoo.com>
2024-10-28 10:20:55 +0000

    Script print number 10 time

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print number 10 times.
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+char number;
+char compteur;
+for (compteur = 0; compteur <= 10; number++)
+{
+for (number = 0; number <= 14; number++)
+{
+_putchar('0' + number);
+}
+_putchar('\n');
+}
+}
