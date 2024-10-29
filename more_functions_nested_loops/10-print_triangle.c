de2bddefc2684cc933fb0c15f88cd3250a4f60c8
root <djelloulrouba@yahoo.com>
2024-10-29 09:23:40 +0000

    Script print triangle with #

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -12,6 +12,7 @@ void print_triangle(int size)
 {
 int compteur = 0;
 int space = 0;
+int nbrhash = size - 1;
 if (size <= 0)
 {
 _putchar('\n');
@@ -25,8 +26,13 @@ while (space < size)
 _putchar(' ');
 space++;
 }
+ while (nbrhash < size)
+{
 _putchar('#');
+nbrhash++;
+}
 _putchar('\n');
+nbrhash = size - space;
 compteur++;
 space = 0;
 }
