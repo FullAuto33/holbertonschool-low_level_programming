4084c659191985f62f000c2bf3c21eed803b190b
root <djelloulrouba@yahoo.com>
2024-10-29 14:40:34 +0000

    Script reverse str

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - print str reverse
+ *
+ *@s : str
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+char letters;
+int nbrtab = 0;
+int compteur = 0;
+for (; s[nbrtab] != 0; nbrtab++)
+{
+compteur = compteur + 1;
+}
+for (; s[compteur] != 0; compteur--)
+{
+letters = s[compteur];
+_putchar(letters);
+}
+_putchar('\n');
+}
