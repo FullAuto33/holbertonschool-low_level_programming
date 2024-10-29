51a446dfebdd365dfa889defc2755cc674f72af6
root <djelloulrouba@yahoo.com>
2024-10-29 14:08:20 +0000

    Script print all str

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - print str
+ *
+ * @str : str to print
+ *
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+int nbrtab = 0;
+for (; str[nbrtab] != 0; nbrtab++)
+{
+_putchar(*str);
+}
+}
