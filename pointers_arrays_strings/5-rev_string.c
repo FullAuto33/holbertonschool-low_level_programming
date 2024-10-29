7d8593a6ca92dfaa78f53fd23a9efd6828d9f68f
root <djelloulrouba@yahoo.com>
2024-10-29 14:58:30 +0000

    Script reverse str

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverse the string
+ *
+ *@s : string
+ *
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+char letters;
+int nbrtab = 0;
+int compteur = 0;
+for (; s[nbrtab] != 0; nbrtab++)
+{
+compteur = compteur + 1;
+}
+for (compteur = compteur - 1; compteur >= 0; compteur--)
+{
+letters = s[compteur];
+}
+printf("%c", letters);
+}
