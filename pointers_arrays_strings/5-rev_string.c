4b273979c7795c6124f3a9346f78f66ab92bb1ae
root <djelloulrouba@yahoo.com>
2024-10-30 09:41:55 +0000

    Script reverse str

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -14,19 +14,14 @@ void rev_string(char *s)
 char letters;
 int nbrtab = 0;
 int compteur = 0;
-int compteur2 = 0;
 for (; s[nbrtab] != 0; nbrtab++)
 {
 compteur = compteur + 1;
 }
 while (compteur >= 0)
 {
-  /*printf("%d", compteur);*/
 letters = s[compteur];
 printf("%c", letters);
-s[compteur2] = s[nbrtab - compteur2 - 1];
-s[nbrtab - compteur2 - 1] = letters;
-compteur2++;
 compteur = compteur - 1;
 }
 }
