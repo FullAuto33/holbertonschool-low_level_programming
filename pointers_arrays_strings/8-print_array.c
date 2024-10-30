1136bbbfb2068a5235ce80154fbcc07dc6fbda04
root <djelloulrouba@yahoo.com>
2024-10-30 15:08:09 +0000

    Script print array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * print_array - print array
+ *
+ *@a : array
+ *
+ *@n : integer
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+int compteur = 0;
+int arrays;
+for (; compteur < n; compteur++)
+{
+arrays = a[compteur];
+printf("%d", arrays);
+}
+}
