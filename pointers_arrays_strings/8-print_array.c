3fd936598691812e1d83f4d56031fba4059a69ee
root <djelloulrouba@yahoo.com>
2024-10-30 15:44:05 +0000

    Script print arrays

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -15,6 +15,14 @@ void print_array(int *a, int n)
 {
 int compteur = 0;
 int arrays;
+if (n < 1)
+{
+compteur = 0;
+}
+else
+{
+compteur = 1;
+}
 for (; compteur < n; compteur++)
 {
 arrays = a[compteur];
