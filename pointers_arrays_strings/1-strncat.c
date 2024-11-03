76d953c8a9c8504cf6fdfd1b3a597d71c64608d6
root <djelloulrouba@yahoo.com>
2024-11-03 20:31:58 +0000

    Script concatenante

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate str
+ * @dest : str concatenante
+ * @src : str two concatenante
+ * @n :  integer
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int compteur1 = 0;
+int compteur2 = 0;
+
+for (; dest[compteur1] != '\0'; compteur1++)
+{
+}
+for (; compteur1 < n && src[compteur2] != '\0'; compteur2++)
+{
+dest[compteur1 + compteur2] = src[compteur2];
+}
+return (dest);
+}
