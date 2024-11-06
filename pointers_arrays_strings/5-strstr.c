fa2785d1117cf5f249a7589613172d3e9020aba5
root <djelloulrouba@yahoo.com>
2024-11-06 10:06:15 +0000

    Script find str in str

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -3,29 +3,33 @@
 /**
  * _strstr - lcoates a substring
  * @haystack: str to search it
- * @neddle: str to find
+ * @needle: str to find
  *
  * Return: Always 0.
  */
 char *_strstr(char *haystack, char *needle)
 {
-  	int compteur = 0;
-	int compteur2 = 0;
+int compteur = 0;
+int compteur1 = 0;
 
-	while (needle[compteur] != '\0')
-		compteur++;
-
-	while (haystack != '\0')
-	{
-		for (; needle[compteur2]; compteur2++)
-		{
-			if (haystack[compteur2] != needle[compteur2])
-			  return (0);
-		}
-		if (compteur2 != compteur)
-			haystack++;
-		else
-			return (haystack);
-	}
-	return (0);
+if (*needle == '\0')
+{
+return (haystack);
+}
+else
+{
+for (; haystack[compteur] != '\0'; compteur++)
+{
+for (;
+needle[compteur1] != '\0' &&
+haystack[compteur + compteur1] == needle[compteur1]; compteur1++)
+{
+}
+if (needle[compteur1] == '\0')
+{
+return (&haystack[compteur]);
+}
+}
+return (haystack + compteur);
+}
 }
