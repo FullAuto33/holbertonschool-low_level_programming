d89435504c8c0f2b07399fcd4b84610e7abcccad
root <djelloulrouba@yahoo.com>
2024-11-06 14:50:58 +0000

    String print str reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print str in reverse
+ *@s: str
+ *
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (s[0] != '\0')
+{
+_puts_recursion(s + 1);
+_putchar(s[0]);
+}
+}
