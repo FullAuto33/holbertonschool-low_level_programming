6fbea3b3cc1c2a6cf3aa92c81c731ae649391e79
root <djelloulrouba@yahoo.com>
2024-11-07 09:07:02 +0000

    Script prime number no finish

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -6,7 +6,31 @@
  * Return: Always 0.
  */
 
+int prime(int n, int n2)
+{
+if (n % n2 == 0)
+{
+return (0);
+}
+return (prime(n, n2 + 1));
+}
+
 int is_prime_number(int n)
 {
-  
+if (n <= 1)
+{
+return (0);
+}
+if (n == 2)
+{
+return (1);
+}
+else if (n % 2 == 0)
+{
+return (0);
+}
+else
+{
+return (prime(n, 3));
+}
 }
