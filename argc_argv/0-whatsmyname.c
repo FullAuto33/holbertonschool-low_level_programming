21928cfbd37f782c39ea156eb08a3267c3dce9b6
root <djelloulrouba@yahoo.com>
2024-11-08 12:46:40 +0000

    Script file print name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print name file
+ * @argc: arg counter
+ * @argv: arg array
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
