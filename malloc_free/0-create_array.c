c25b9c988b4d645a4c19147710e19b7ddad91fbc
root <djelloulrouba@yahoo.com>
2024-11-12 09:40:05 +0000

    Script create array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create a array
+ * @c: char
+ * @size: size of array
+ *
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+unsigned int compteur = 0;
+char *ar;
+ar = malloc(size * sizeof(*ar));
+
+if (size == 0)
+{
+return (NULL);
+}
+for (; compteur < size; compteur++)
+{
+ar[compteur] = c;
+}
+return (ar);
+}
