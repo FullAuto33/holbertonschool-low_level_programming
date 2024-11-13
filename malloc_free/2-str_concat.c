6835bd765208989b2ba2ccfdc2dd901ad23f2875
root <djelloulrouba@yahoo.com>
2024-11-13 10:16:42 +0000

    Script concaten two str

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,5 +11,34 @@
 
 char *str_concat(char *s1, char *s2)
 {
-  
+char *ar;
+int tailles1 = 0;
+int tailles2 = 0;
+int compteur = 0;
+int compteurs2 = 0;
+
+for (; s1[tailles1] != '\0'; tailles1++)
+{
+}
+for (; s2[tailles2] != '\0'; tailles2++)
+{
+}
+ar = malloc(tailles1 * sizeof(*s1) + tailles2 * sizeof(*s2));
+if (ar == NULL)
+{
+return (NULL);
+}
+for (; compteur < tailles1 + tailles2; compteur++)
+{
+if (compteur < tailles1)
+{
+ar[compteur] = s1[compteur];
+}
+else
+{
+ar[compteur] = s2[compteurs2];
+compteurs2++;
+}
+}
+return (ar);
 }
