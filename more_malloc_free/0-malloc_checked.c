d492e737920b9eb9a44a0246f980dddb3bb4c3db
root <djelloulrouba@yahoo.com>
2024-11-14 09:02:51 +0000

    Script more malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocated memroy with malloc
+ *@b: int unsigned
+ *
+ * Return: Always 0.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+char *ptr;
+ptr = malloc(sizeof(b));
+if (ptr == NULL)
+{
+return (NULL);
+}
+return (ptr);
+}
