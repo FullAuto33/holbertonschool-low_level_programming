d3e6a32af82e7aac6d8093299a37a9278c99bf66
root <djelloulrouba@yahoo.com>
2024-11-15 15:24:37 +0000

    Script typedefstructure dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - initialize  dog
+ * @d: ptr dog
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner dog
+ * Return: dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d == NULL)
+return;
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
