477f5eb5b93445cc63cab5f448c1a9f952b88735
root <djelloulrouba@yahoo.com>
2024-11-15 15:47:34 +0000

    Script new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * new_dog - new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
