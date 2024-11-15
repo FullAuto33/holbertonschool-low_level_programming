261d42fccfb203252a1ed6f0002459feda16a1ba
root <djelloulrouba@yahoo.com>
2024-11-15 15:21:23 +0000

    Script typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,17 @@
-typedef struct dog
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-}
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
