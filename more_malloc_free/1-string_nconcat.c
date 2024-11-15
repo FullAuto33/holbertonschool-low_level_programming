af0c8ea1441da13b9e41bdfc155dc93458c11aff
root <djelloulrouba@yahoo.com>
2024-11-15 15:05:22 +0000

    Script moremaloc concatstr

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -5,19 +5,17 @@
  * string_nconcat - Concatenates two strings
  * @s1: First string
  * @s2: Second string
- * @n: Number of bytes from s2 to include
+ * @n: number of array woth s2
  *
- * Return: Pointer to newly allocated memory with concatenated strings,
- *         or NULL if memory allocation fails.
+ * Return: ptr
  */
-char* string_nconcat(char* s1, char* s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *ptr;
 unsigned int tailles1 = 0;
-unsigned int tailles2 = 0; 
-unsigned int i; 
+unsigned int tailles2 = 0;
+unsigned int i;
 unsigned int j;
-
 if (s1 == NULL)
 {
 s1 = "";
@@ -26,7 +24,6 @@ if (s2 == NULL)
 {
 s2 = "";
 }
-
 while (s1[tailles1] != '\0')
 {
 tailles1++;
@@ -35,25 +32,19 @@ while (s2[tailles2] != '\0')
 {
 tailles2++;
 }
-
 if (n >= tailles2)
 {
 n = tailles2;
 }
-
 ptr = malloc((tailles1 + n + 1) * sizeof(char));
 if (ptr == NULL)
 {
 return (NULL);
 }
-
-
 for (i = 0; i < tailles1; i++)
 {
 ptr[i] = s1[i];
 }
-
-
 for (j = 0; j < n; j++)
 {
 ptr[i + j] = s2[j];
