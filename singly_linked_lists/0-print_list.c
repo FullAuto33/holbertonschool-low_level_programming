042de8baae3f0cec42c353e169e32e292794ab80
root <djelloulrouba@yahoo.com>
2024-12-04 09:03:22 +0000

    print list task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,22 @@
+#include "lists.h"
 #include <stdio.h>
-
+#include <stddef.h>
 
 size_t print_list(const list_t *h)
 {
+size_t compteur = 0;
+while ( h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%d] %s\n",h->len, h->str);
+}
+h = h->next;
+compteur++;
+}
+ return (compteur);
 }
