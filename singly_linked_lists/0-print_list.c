531065883df4bedd3393febda800d8e37defa662
root <djelloulrouba@yahoo.com>
2024-12-04 09:06:21 +0000

    print list task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -2,10 +2,16 @@
 #include <stdio.h>
 #include <stddef.h>
 
+/**
+ * print_list - print list
+ * @h: pointer list
+ * Return: Always 0.
+ */
+
 size_t print_list(const list_t *h)
 {
 size_t compteur = 0;
-while ( h != NULL)
+while (h != NULL)
 {
 if (h->str == NULL)
 {
@@ -13,10 +19,10 @@ printf("[0] (nil)\n");
 }
 else
 {
-printf("[%d] %s\n",h->len, h->str);
+printf("[%d] %s\n", h->len, h->str);
 }
 h = h->next;
 compteur++;
 }
- return (compteur);
+return (compteur);
 }
