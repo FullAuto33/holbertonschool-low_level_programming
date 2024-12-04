c9de05f449b330040bcd16a29f2fd919aad9b494
root <djelloulrouba@yahoo.com>
2024-12-04 09:51:00 +0000

    Script new node without strlen with for

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,12 +14,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *node = malloc(sizeof(list_t));
+int compteur = 0;
+const char *lenght = str;
+for (; *lenght != '\0'; lenght++)
+{
+compteur++;
+}
 if (node == NULL)
 {
 return (NULL);
 }
 node->str = strdup(str);
-node->len = strlen(str);
+node->len = compteur;
 node->next = *head;
 *head = node;
 return (*head);
