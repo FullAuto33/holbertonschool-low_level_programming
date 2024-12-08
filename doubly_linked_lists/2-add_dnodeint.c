90d41860e224c51df7e83e2f9dd4e66ee34f9501
root <djelloulrouba@yahoo.com>
2024-12-08 15:23:07 +0000

    add description to function add node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,13 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * add_dnodeint - add node in list
+ * @head: pointer double of head
+ * @n: int to node
+ * Return: Always EXIT_SUCCESS.
+ */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *noeud;
