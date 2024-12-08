5f9e21976fb93af4e4e63c09f1b4c4551965fd69
root <djelloulrouba@yahoo.com>
2024-12-08 14:52:41 +0000

    Script count number of element in list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - number of element in list
+ * @h: pointer header
+ * Return: Always EXIT_SUCCESS.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t compteur = 0;
+while (h != NULL)
+{
+compteur++;
+h = h->next;
+}
+return(compteur);
+}
