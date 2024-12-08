21140dac89406360d2b8c2594f8765e091f1c002
root <djelloulrouba@yahoo.com>
2024-12-08 16:50:49 +0000

    Script delete node of index

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - go to index and delete the node
+ *@head: double pointer to head
+ * @index: int index
+ * Return: Always EXIT_SUCCESS.
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+dlistint_t *noeud = *head;
+
+if (*head == NULL)
+{
+return (-1);
+}
+for (; index != 0; index--)
+{
+if (neoud == NULL)
+{
+return (-1);
+}
+noeud = noeud->next;
+}
+
+if (noeud == *head)
+{
+*head = noeud->next;
+if (*head != NULL)
+{
+(*head)->prev = NULL;
+}
+}
+
+else
+{
+noeud->prev->next = tmp->next;
+if (noeud->next != NULL)
+noeud->next->prev = noeud->prev;
+}
+
+free(noeud);
+return (1);
+}
